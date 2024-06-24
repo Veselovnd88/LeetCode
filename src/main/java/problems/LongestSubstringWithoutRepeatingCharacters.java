5d520b1fd83e4593e4e834cd5622b541242cf9e4
Veselov Nikolay <veselovnd@gmail.com>
2024-06-24 11:00:37 +0300

    Longest Substring Without Repeating Characters №2

--- src/main/java/problems/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/problems/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author VeselovND <br>
  * created at 24.06.2024 <br>
@@ -13,11 +16,38 @@ public class LongestSubstringWithoutRepeatingCharacters {
         System.out.println(lengthOfLongestSubstring("pwwkew"));
     }
 
+    //fast
     public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int left = 0;
+        int right = 0;
+        int max = 0;
+
+        while (right < s.length()) {
+            Integer index = map.get(s.charAt(right)); //ищем индекс текущего символа
+            if (index == null) {
+                max = Math.max(max, right - left + 1); //если индекса еще нет, то обновляем максимум, и вставляем в мапу символ с индексом
+                map.put(s.charAt(right), right);
+                right++;
+                //если индекс уже есть (=повтор символа), то смотрим, если индекс в пределах диапазона то сдвигаем сабстрингу
+            } else {
+                if (index >= left && index < right) {
+                    left = index + 1;
+                }
+                max = Math.max(max, right - left + 1); //обновляем максимум
+                map.put(s.charAt(right), right); //изменили индекс
+                right++;
+            }
+        }
+        return max;
+    }
+
+    public static int lengthOfLongestSubstringSlow(String s) {
         if (s.isEmpty()) {
             return 0;
         }
-        if (s.length()==1) {
+        if (s.length() == 1) {
             return 1;
         }
         int slow = 0;
